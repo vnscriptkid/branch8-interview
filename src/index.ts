7d810268cf92bd8694fbe4474257ebea9536b768
Thanh Nguyen <vnscriptkid@gmail.com>
2022-05-30 18:00:58 +0700

    feat: setup db

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import { connectDb } from "./db";
 
 const app = express();
 
@@ -6,8 +7,21 @@ app.get("/api/status", (req, res) => {
   return res.send({ alive: true });
 });
 
+app.post("/api/auth/register", async (req, res) => {
+  return res.send({ done: true });
+});
+
+app.post("/api/auth/login", async (req, res) => {
+  return res.send({ done: true });
+});
+
+app.get("/api/auth/me", async (req, res) => {
+  return res.send({ done: true });
+});
+
 const PORT = 8085;
 
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
+  await connectDb();
   console.log(`server is listening on port ${PORT}`);
 });
